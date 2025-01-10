bf575082c9cc74a67756f772a945d16fc8b57c77
Holly Morris <hollymorris@Hollys-MacBook-Air.local>
2025-01-10 19:22:57 +0000

    Refractors and optimises current code

--- db_utils.py
+++ db_utils.py
@@ -1,4 +1,3 @@
-#%%
 import yaml 
 from sqlalchemy import create_engine
 import pandas as pd
@@ -13,26 +12,30 @@ class RDSDatabaseConnector:
         self.credentials_data = credentials_data
 
     def initialise_sqlalchemy(self):
-        DATABASE_TYPE = 'postgresql'
-        DBAPI = 'psycopg2'
-        USER = self.credentials_data['RDS_USER']
-        PASSWORD = self.credentials_data['RDS_PASSWORD']
-        HOST = self.credentials_data['RDS_HOST']
-        PORT = self.credentials_data['RDS_PORT']
-        DATABASE = self.credentials_data['RDS_DATABASE']
-        self.engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
-        self.engine.connect()
+        try:
+            DATABASE_TYPE = 'postgresql'
+            DBAPI = 'psycopg2'
+            USER = self.credentials_data['RDS_USER']
+            PASSWORD = self.credentials_data['RDS_PASSWORD']
+            HOST = self.credentials_data['RDS_HOST']
+            PORT = self.credentials_data['RDS_PORT']
+            DATABASE = self.credentials_data['RDS_DATABASE']
+            self.engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
+            self.engine.connect()
+            print("Database connection established.")
+        except Exception as e:
+            print(f"Failed to connect to database: {e}")
     
     def extract_data(self):
         extracted_data = pd.read_sql_table('loan_payments', self.engine)
         return extracted_data
     
-def save_data_to_csv(data_df, file_path='/Users/hollymorris/Documents/loan_payments_data.csv'):
-    data_df.to_csv('/Users/hollymorris/Documents/loan_payments_data.csv', index=False)
+def save_data_to_csv(data_df, file_path='/Users/hollymorris/Python/exploratory-data-analysis---customer-loans-in-finance994/loan_payments_data.csv'):
+    data_df.to_csv(file_path, index=False)
 
 def load_data_into_pandas_df():
-    loan_payments_df = pd.read_csv('/Users/hollymorris/Documents/loan_payments_data.csv')
-    loan_payments_df.head(4)
+    loan_payments_df = pd.read_csv('/Users/hollymorris/Python/exploratory-data-analysis---customer-loans-in-finance994/loan_payments_data.csv')
+    print(loan_payments_df.head(4))
     shape = loan_payments_df.shape
     print(f'This dataset has {shape[0]} rows and {shape[1]} columns')
     loan_payments_df.info()
@@ -40,7 +43,6 @@ def load_data_into_pandas_df():
 credentials = load_credentials('/Users/hollymorris/Python/exploratory-data-analysis---customer-loans-in-finance994/credentials.yaml')
 connector = RDSDatabaseConnector(credentials)
 connector.initialise_sqlalchemy()
-data_df = connector.extract_data()
-save_data_to_csv(data_df, 'loan_payments_data.csv')
+loan_payments_df = connector.extract_data()
+save_data_to_csv(loan_payments_df, 'loan_payments_data.csv')
 load_data_into_pandas_df()
-# %%
